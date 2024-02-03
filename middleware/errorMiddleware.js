cb0b15a12ddf6db9316b14582f8954e0a082b078
Nitish Kumar <nitkumar15@publicisgroupe.net>
2024-02-03 20:37:18 +0530

    new component

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,7 +2,6 @@ const errorMiddleware = (err, req, res, next) => {
    
     const statusCode = res.statusCode ? res.statusCode : 500;
     res.status(statusCode);
-    console.log(process.env.NODE_ENV);
     res.json({message:err.message, stack: process.env.NODE_ENV === "dev" ? err.stack : null});
 }
 
