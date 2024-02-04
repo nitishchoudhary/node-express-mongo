223441f398bf95403ba61489fd439b654d11282e
Nitish Kumar <nitkumar15@publicisgroupe.net>
2024-02-04 12:02:55 +0530

    code update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
+const { error } = require('console');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
 
 const getUser = asyncHandler(async(req, res) => {
     try {
-        const userData = await User.find({});
+        const userData = await User.findOne();
         res.status(200).json(userData);
     } catch (error) {
         res.status(500);
@@ -14,11 +15,31 @@ const getUser = asyncHandler(async(req, res) => {
 
 const createUser = asyncHandler(async(req, res) => {
     try {
+        const {name, email, password} = req.body;
+        //check name not empty
+        if(!name){
+            return res.json({
+                error: "name is required"
+            })
+        }
+        //check password 
+        if(!password && password.lenght < 8){
+            return res.json({
+                error: 'Password is required & lenght should be at least 8 character'
+            })
+        }
+        //check email
+        const emailExists = await User.findOne({email});
+        if(emailExists){
+            return res.json({
+                error: 'email is already taken'
+            })
+        }
         const userData = await User.create(req.body);
         res.status(200).json(userData);
     } catch (error) {
         res.status(500);
-        throw new Error(error.message);
+        throw new Error(error);
     }
 });
 
--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,7 +2,8 @@ const errorMiddleware = (err, req, res, next) => {
    
     const statusCode = res.statusCode ? res.statusCode : 500;
     res.status(statusCode);
-    res.json({message:err.message, stack: process.env.NODE_ENV === "dev" ? err.stack : null});
+    res.json(err);
+    //res.json({message:err.message, stack: process.env.NODE_ENV === "dev" ? err.stack : null});
 }
 
 module.exports = errorMiddleware;
